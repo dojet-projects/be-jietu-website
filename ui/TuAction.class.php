136400199021a8eed2b09da6ff4939706b0fc307
liyan34 <liyan34@lenovo.com>
2016-05-03 19:58:56 +0800

    avatar

--- ui/TuAction.class.php
+++ ui/TuAction.class.php
@@ -8,17 +8,32 @@
  */
 class TuAction extends XBaseAction {
 
-    public function execute() {
-        $img1 = new MImage(DATA.'image/1.png');
-        $img2 = new MImage(DATA.'image/2.png');
-        $this->addText($img1, $img2, 'hello', true);
-        // $img1->copy($img2, 100, 100, 0, 0, 100, 100);
+    protected $chat_y;
+    protected $bg;
 
-        $img1->display();
+    function __construct() {
+        $this->chat_y = 40 + 88 + 88;
     }
 
-    public function addText(MImage $img, MImage $avatar, $text, $isMe) {
-        $img->ttftext('中国移动', DATA.'font/simhei.ttf', 18, 88, 29, 0, 0xffffff);
+    public function execute() {
+        $this->bg = new MImage(DATA.'image/1.png');
+        $avatar = new MImage(DATA.'image/avatar.jpg');
+        $this->setCarrier('中国移动');
+        $this->addChat($avatar, 'hello', false);
+
+        // $bg->copy($img2, 100, 100, 0, 0, 100, 100);
+
+        $this->bg->display();
+    }
+
+    public function setCarrier($carrier) {
+        $this->bg->ttftext($carrier, DATA.'font/simhei.ttf', 18, 88, 29, 0, 0xffffff);
+    }
+
+    public function addChat(MImage $avatar, $text, $isMe) {
+        $avatar->resize(80, 80);
+        $a_x = 20;
+        $this->bg->copy($avatar, $a_x, $this->chat_y, 0, 0, 80, 80);
     }
 
 }
