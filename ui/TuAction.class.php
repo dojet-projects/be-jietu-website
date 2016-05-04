11a7934d716f092ec8e2a644c9b54c1099a8d092
liyan34 <liyan34@lenovo.com>
2016-05-04 13:32:53 +0800

    chat

--- ui/TuAction.class.php
+++ ui/TuAction.class.php
@@ -16,8 +16,8 @@ class TuAction extends XBaseAction {
     }
 
     public function execute() {
-        $this->bg = new MImage(DATA.'image/1-1.png');
-        $avatar = new MImage(DATA.'image/avatar.jpg');
+        $this->bg = MImage::imageFromFile(DATA.'image/1-1.png');
+        $avatar = MImage::imageFromFile(DATA.'image/avatar.jpg');
         $this->setCarrier('中国移动');
         $this->addChat($avatar, 'hello', false);
         $this->setSignal(2);
@@ -30,13 +30,13 @@ class TuAction extends XBaseAction {
 
     public function setSignal($n = 5) {
         $path = sprintf(DATA.'image/signal-%s.png', $n);
-        $img = new MImage($path);
+        $img = MImage::imageFromFile($path);
         $this->bg->copy($img, 0, 0, 0, 0, 88, 40);
     }
 
     public function setNetwork($network, $n) {
         $path = sprintf(DATA.'image/network-%s-%s.png', $network, $n);
-        $img = new MImage($path);
+        $img = MImage::imageFromFile($path);
         $this->bg->copy($img, 190, 0, 0, 0, 32, 40);
     }
 
@@ -52,6 +52,11 @@ class TuAction extends XBaseAction {
         $avatar->resize(80, 80);
         $a_x = 20;
         $this->bg->copy($avatar, $a_x, $this->chat_y, 0, 0, 80, 80);
+
+        $chatbox = MImage::imageFromFile(DATA.'image/chat-ta.png');
+        $chatbox->resize9(500, 300, 40, 30, 180, 22);
+
+        $this->bg->copy($chatbox, 120, $this->chat_y, 0, 0, $chatbox->width(), $chatbox->height());
     }
 
 }
